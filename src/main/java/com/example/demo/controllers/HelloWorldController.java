69a7707f2ec5853326bfeff30f6c70d5dd1a76a3
Jose Flores <josecarlosfloresmoran@hotmail.com>
2021-05-18 10:35:05 -0600

    preautorize role en los servicios

--- src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @RequestMapping("hello")
-    @PreAuthorize("hasRole()")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public String helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
         return "Hello "+name+"!!";
     }
